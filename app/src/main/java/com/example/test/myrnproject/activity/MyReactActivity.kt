9503fe92a379ef5269743062099b32f87f5086d9
longfeng <ex-longfeng@xianglin.cn>
2018-06-19 16:16:03 +0800

    java -> kotlin

---
+++ app/src/main/java/com/example/test/myrnproject/activity/MyReactActivity.kt
@@ -0,0 +1,93 @@
+package com.example.test.myrnproject.activity
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.KeyEvent
+
+import com.example.test.myrnproject.BuildConfig
+import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactRootView
+import com.facebook.react.common.LifecycleState
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
+import com.facebook.react.shell.MainReactPackage
+
+/**
+ * 【类功能说明】
+ * ...
+ * File: MyReactActivity.java
+ * Author: longfeng
+ * Vesion: 3.2.0
+ * Create: 2018/6/15
+ * Changes (from 2018/6/15)
+ * -------------------------------------------------------
+ * 2018/6/15:创建MyReactActivity.java(longfeng)
+ * -------------------------------------------------------
+ */
+class MyReactActivity : Activity(), DefaultHardwareBackBtnHandler {
+    private var mReactRootView: ReactRootView? = null
+    private var mReactInstanceManager: ReactInstanceManager? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mReactRootView = ReactRootView(this)
+        mReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(application)
+                .setBundleAssetName("index.android.bundle")
+                .setJSMainModulePath("index.android")
+                .addPackage(MainReactPackage())
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .build()
+
+        // 注意这里的MyReactNativeApp必须对应“index.js”中的
+        // “AppRegistry.registerComponent()”的第一个参数
+        mReactRootView!!.startReactApplication(mReactInstanceManager, "MyReactNativeApp", null)
+
+        setContentView(mReactRootView)
+    }
+
+    override fun invokeDefaultOnBackPressed() {
+        super.onBackPressed()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager!!.onHostPause(this)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager!!.onHostResume(this, this)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager!!.onHostDestroy()
+        }
+    }
+
+    override fun onBackPressed() {
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager!!.onBackPressed()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
+            mReactInstanceManager!!.showDevOptionsDialog()
+            return true
+        }
+        return super.onKeyUp(keyCode, event)
+    }
+}
